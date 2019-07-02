99acb133d42fec39af8baa9ac4e6d5d4979c5804
Brian <brian770130@gmail.com>
2019-07-02 17:08:14 +0800

    add getBlock by height

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const jayson = require('jayson')
 const app = express()
 const env = require('./env');
 const request = require('request');
+const moment = require('moment');
 
 var evmliteAPI = 'http://' + env.apiHost + ':' + env.evmlitePort;
 var tendermintAPI = 'http://' + env.apiHost + ':' + env.tendermintPort;
@@ -11,7 +12,7 @@ var tendermintAPI = 'http://' + env.apiHost + ':' + env.tendermintPort;
 const server = jayson.server({
   net_listening: (args, callback) => {
     // web3.eth.net.isListening
-    console.log("curl http://[api_addr]/info");
+    console.log("Get /isListening");
     request.get(evmliteAPI + '/info', (err, rep) => {
       if (err) {
         console.log(err);
@@ -29,7 +30,7 @@ const server = jayson.server({
   },
   eth_coinbase: (args, callback) => {
     // web3.eth.getCoinbase
-    console.log("curl http://[api_addr]/accounts");
+    console.log("GET /getCoinbase");
     request.get(evmliteAPI + '/accounts', (err, rep) => {
       if (err) {
         console.log(err);
@@ -42,7 +43,7 @@ const server = jayson.server({
   },
   eth_blockNumber: (args, callback) => {
     // web3.eth.getBlockNumber
-    console.log("curl http://[api_addr]/block");
+    console.log("GET /getBlockNumber");
     request.get(tendermintAPI + '/block', (err, rep) => {
       if (err) {
         console.log(err);
@@ -55,8 +56,24 @@ const server = jayson.server({
     });
   },
   eth_getBlockByNumber: (args, callback) => {
-    callback(null, true);
-    console.log("web3.eth.getBlock");    
+    // web3.eth.getBlock
+    console.log("POST /getBlock");
+    var height = parseInt(args[0]);
+    request.get(tendermintAPI + '/block?height=' + height, (err, rep) => {
+      if (err) {
+        console.log(err);
+      } else {
+        var body = JSON.parse(rep.body);
+        var tendermintblock = body.result.block;
+        var timestamp = moment(tendermintblock.header.time).unix();
+        var returnBlock = {
+          "number": height.toString(16),
+          "timestamp": timestamp.toString(16),
+          "transactions": tendermintblock.data.txs,
+        }
+        callback(null, returnBlock);
+      }
+    });
   },
   eth_sendRawTransaction: (args, callback) => {
     callback(null, true);
@@ -76,6 +93,7 @@ app.use(bodyParser.json())
 app.use(server.middleware());
 
 app.listen(8545, () => {
-  console.log('tendermint node is listening on http://' + env.tendermintHost + ":" + env.tendermintPort);
+  console.log('tendermint node is listening on port ' + env.tendermintPort);
+  console.log('EVM_lite is listening on port '+ env.evmlitePort)
   console.log('RPC server is listening on port 8545');
 });
